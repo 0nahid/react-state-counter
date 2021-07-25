fba19cd60d73ad1d06ff46ce3a5e8044c20cd8a1
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-07-25 11:38:28 +0600

    add counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const user = [
+    { name : 'Bunny', mail:'seisei@gmail.com'},
+    { name : 'Nahid', mail:'nahid@gmail.com'},
+    { name : 'Hassan', mail:'hassan@gmail.com'},
+    { name : 'Bulbul', mail:'bulbul@gmail.com'},
+    { name : 'Bunny', mail:'bunny@gmail.com'},
+    { name : 'Robin', mail:'robin@gmail.com'},
+    { name : 'Miya', mail:'miya@gmail.com'},
+    { name : 'Md', mail:'md@gmail.com'},
+    { name : 'Example', mail:'example@gmail.com'},
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter> </Counter>
+        {user.map( person => <Person person={person} ></Person> )}
       </header>
     </div>
-  );
+  )
 }
-
+function Person({person}){
+  return (
+    <div className='personStyle'>
+      <h1>Name : {person.name} </h1>
+      <h4>Mail : {person.mail} </h4>
+    </div>
+  )
+}
+function Counter(){
+  const [count, setCount] = useState(0);
+  return(
+    <div className="counterBtn">
+      <h1>Count:{count}</h1>
+      <button onClick={() => setCount(count - 1) }>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
 export default App;
